02cc84ba8f19d72fe0b0cf0982359206b3f8f420
ahmedadhem <ahmedadhem330@gmail.com>
2025-02-02 12:15:44 +0200

    Fixed bug in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Home from './components/home/Home'
 import About from './components/about/About'
-import Contact from './components/contact/contact'
+import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Nav from './components/nav/Nav'
 import Project from './components/project/Project'
